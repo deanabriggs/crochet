087e896706d93dc186012969f717bb080b5efceb
Deana Briggs <145316150+deanabriggs@users.noreply.github.com>
2024-11-25 08:56:00 -0800

    removed http & basePath from swagger.js

--- swagger.js
+++ swagger.js
@@ -1,18 +1,17 @@
 // Require resources
-const swaggerAutogen = require('swagger-autogen')();
+const swaggerAutogen = require("swagger-autogen")();
 
 const doc = {
-    info: {
-        title: 'Crochet Projects API',
-        description: 'Crochet Projects API'
-    },
-    host: 'crochet-1wd1.onrender.com',
-    basePath: "",
-    schemes: ['http', 'https']
+  info: {
+    title: "Crochet Projects API",
+    description: "Crochet Projects API",
+  },
+  host: "crochet-1wd1.onrender.com",
+  schemes: "https",
 };
 
-const outputFile = './swagger.json';
-const endpointsFiles = ['./routes/index.js'];
+const outputFile = "./swagger.json";
+const endpointsFiles = ["./routes/index.js"];
 
 // generates swagger.json
 swaggerAutogen(outputFile, endpointsFiles, doc);
