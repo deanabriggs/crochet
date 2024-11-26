68f5eba7ddb93bd6fd99b26c0b2e3312ef2d2600
Deana Briggs <145316150+deanabriggs@users.noreply.github.com>
2024-11-25 21:59:38 -0800

    removed api-docs from swagger.json

--- controllers/projects.js
+++ controllers/projects.js
@@ -1,83 +1,105 @@
 // Require resources
-const mongodb = require('../data/database');
-const ObjectId = require('mongodb').ObjectId;
+const mongodb = require("../data/database");
+const ObjectId = require("mongodb").ObjectId;
 
 // Define functions for obtaining specific data
 const getAllProjects = async (req, res) => {
-    // #swagger.tags=['Projects']
-    const result = await mongodb.getDb().db().collection('projects').find();
-    result.toArray().then((projects) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(projects);
-    });
+  // #swagger.tags=['Projects']
+  const result = await mongodb.getDb().db().collection("projects").find();
+  result.toArray().then((projects) => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json(projects);
+  });
 };
 
 // Define functions for obtaining specific data
 const getSingleProject = async (req, res) => {
-    // #swagger.tags=['Projects']
-    const projectId = new ObjectId(req.params.id);
-    const result = await mongodb.getDb().db().collection('projects').find({_id: projectId});
-    result.toArray().then((projects) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(projects[0]);
-    });
+  // #swagger.tags=['Projects']
+  const projectId = new ObjectId(req.params.id);
+  const result = await mongodb
+    .getDb()
+    .db()
+    .collection("projects")
+    .find({ _id: projectId });
+  result.toArray().then((projects) => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json(projects[0]);
+  });
 };
 
 const createProject = async (req, res) => {
-    // #swagger.tags=['Projects']
-    const project = {
-        title: req.body.title,
-        category: req.body.category,
-        yarnType: req.body.yarnType,
-        yarnQty: req.body.yarnQty,
-        needle: req.body.needle,
-        picture: req.body.picture,
-        pattern: req.body.pattern
-    }
-    const response = await mongodb.getDb().db().collection('projects').insertOne(project);
-    if(response.acknowledged) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.err || 'An error occurred while adding project.');
-    }
+  // #swagger.tags=['Projects']
+  const project = {
+    title: req.body.title,
+    category: req.body.category,
+    yarnType: req.body.yarnType,
+    yarnQty: req.body.yarnQty,
+    needle: req.body.needle,
+    picture: req.body.picture,
+    pattern: req.body.pattern,
+  };
+  const response = await mongodb
+    .getDb()
+    .db()
+    .collection("projects")
+    .insertOne(project);
+  if (response.acknowledged) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(response.err || "An error occurred while adding project.");
+  }
 };
 
 const updateProject = async (req, res) => {
-    // #swagger.tags=['Projects']
-    const projectId = new ObjectId(req.params.id);
-    const project = {
-        title: req.body.title,
-        category: req.body.category,
-        yarnType: req.body.yarnType,
-        yarnQty: req.body.yarnQty,
-        needle: req.body.needle,
-        picture: req.body.picture,
-        pattern: req.body.pattern
-    }
-    const response = await mongodb.getDb().db().collection('projects').replaceOne({_id: projectId}, project);
-    if(response.modifiedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.err || 'An error occurred while updating project.');
-    }
+  // #swagger.tags=['Projects']
+  const projectId = new ObjectId(req.params.id);
+  const project = {
+    title: req.body.title,
+    category: req.body.category,
+    yarnType: req.body.yarnType,
+    yarnQty: req.body.yarnQty,
+    needle: req.body.needle,
+    picture: req.body.picture,
+    pattern: req.body.pattern,
+  };
+  const response = await mongodb
+    .getDb()
+    .db()
+    .collection("projects")
+    .replaceOne({ _id: projectId }, project);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(response.err || "An error occurred while updating project.");
+  }
 };
 
 const deleteProject = async (req, res) => {
-    // #swagger.tags=['Projects']
-    const projectId = new ObjectId(req.params.id);
-    const response = await mongodb.getDb().db().collection('projects').deleteOne({_id: projectId});
-    if(response.deletedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.err || 'An error occured while deleting project.');
-    }
+  // #swagger.tags=['Projects']
+  const projectId = new ObjectId(req.params.id);
+  const response = await mongodb
+    .getDb()
+    .db()
+    .collection("projects")
+    .deleteOne({ _id: projectId });
+  if (response.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(response.err || "An error occured while deleting project.");
+  }
 };
 
 // Export Modules
 module.exports = {
-    getAllProjects,
-    getSingleProject,
-    createProject,
-    updateProject,
-    deleteProject
-}
+  getAllProjects,
+  getSingleProject,
+  createProject,
+  updateProject,
+  deleteProject,
+};
