ef64b81bfd779b6ff7c93fcc464ba1e64483e5ab
Deana Briggs <145316150+deanabriggs@users.noreply.github.com>
2024-11-26 19:08:15 -0800

    Add validation to contributor

--- routes/contributors.js
+++ routes/contributors.js
@@ -1,14 +1,27 @@
 // Requipre resources
-const router = require('express').Router();
-const contributorsCtrl = require('../controllers/contributors');
-const {contributorValidationRules, validate} = require('../validation/validator');
+const router = require("express").Router();
+const contributorsCtrl = require("../controllers/contributors");
+const {
+  contributorValidationRules,
+  validate,
+} = require("../validation/validator");
 
 // Call functions for the specific request
-router.get('/', contributorsCtrl.getAllContributors);
-router.get('/:id', contributorsCtrl.getSingleContributor);
-router.post('/', contributorsCtrl.createContributor);
-router.put('/:id', contributorsCtrl.updateContributor);
-router.delete('/:id', contributorsCtrl.deleteContributor);
+router.get("/", contributorsCtrl.getAllContributors);
+router.get("/:id", contributorsCtrl.getSingleContributor);
+router.post(
+  "/",
+  contributorValidationRules(),
+  validate,
+  contributorsCtrl.createContributor
+);
+router.put(
+  "/:id",
+  contributorValidationRules(),
+  validate,
+  contributorsCtrl.updateContributor
+);
+router.delete("/:id", contributorsCtrl.deleteContributor);
 
 // Export the router
 module.exports = router;
--- validation/validator.js
+++ validation/validator.js
@@ -1,31 +1,35 @@
 // Require resources
-const { body, validationResult } = require('express-validator');
+const { body, validationResult } = require("express-validator");
 
 // Identify needed validation
 const contributorValidationRules = () => {
-    return [
-        body('firstName').isLength({min:3}),
-        body('lastName').isLength({min:3}),
-        body('email').isEmail()
-    ]
-}
+  return [
+    body("firstName").isLength({ min: 3 }),
+    body("lastName").isLength({ min: 3 }),
+    body("email").isEmail(),
+    body("phone").isInt().isLength({ min: 10, max: 10 }),
+    body("joinDate").isDate(),
+    body("numOfProj").isInt(),
+    body("qtyFollows").isInt().optional(),
+  ];
+};
 
 // Checks if there is a validation issue
 const validate = (req, res, next) => {
-    const errors = validationResult(req)
-    if (errors.isEmpty()) {
-        return next()
-    }
-    const extractedErrors = []
-    errors.array().map(err => extractedErrors.push({ [err.param]: err.msg}))
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return next();
+  }
+  const extractedErrors = [];
+  errors.array().map((err) => extractedErrors.push({ [err.param]: err.msg }));
 
-    return res.status(422).json({
-        errors: extractedErrors,
-    })
-}
+  return res.status(422).json({
+    errors: extractedErrors,
+  });
+};
 
 // Export valication functions
 module.exports = {
-    contributorValidationRules,
-    validate
-}
+  contributorValidationRules,
+  validate,
+};
